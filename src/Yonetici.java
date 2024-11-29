c16586331cb14e69bd303f5c668f741b97950b42
MOHAMMED ABDULHAMID ALBAWENDI <107199115+mohammed2004bawendi@users.noreply.github.com>
2024-11-30 02:15:21 +0300

    Update Yonetici.java

--- src/Yonetici.java
+++ src/Yonetici.java
@@ -1,12 +1,42 @@
-public class Yonetici extends Personel{
-    private String yoneticiOzelKimlik;
+import java.util.ArrayList;
 
+public class Manager extends Employee {
+    private ArrayList<Room> rooms; // Oda listesi
 
-    public String getYoneticiOzelKimlik() {
-        return yoneticiOzelKimlik;
+    // Constructor (Yapıcı metod)
+    public Manager(String name, String id) {
+        super(name, id, "Manager");
+        rooms = new ArrayList<>();
     }
 
-    public void setYoneticiOzelKimlik(String yoneticiOzelKimlik) {
-        this.yoneticiOzelKimlik = yoneticiOzelKimlik;
+    // Oda ekleme
+    public void addRoom(Room room) {
+        rooms.add(room);
+        System.out.println("Oda başarıyla eklendi.");
+    }
+
+    // Oda silme
+    public void removeRoom(int roomNumber) {
+        for (Room room : rooms) {
+            if (room.getRoomNumber() == roomNumber) {
+                rooms.remove(room);
+                System.out.println("Oda başarıyla silindi.");
+                return;
+            }
+        }
+        System.out.println("Oda bulunamadı!");
+    }
+
+    // Oda bilgilerini listeleme
+    public void listRooms() {
+        System.out.println("Tüm Odalar:");
+        for (Room room : rooms) {
+            System.out.println(room.roomDetails());
+        }
+    }
+
+    // Oda arama
+    public void searchRoom(int roomNumber) {
+        Room.searchRoomByNumber(roomNumber, rooms);
     }
 }
